838182c7d86413eaf2405c99fea4a9e700bb643d
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2023-06-09 16:22:27 -0300

    feat: ensure Analyzer lowercase tokens

--- src/analyzer/analyzer.ts
+++ src/analyzer/analyzer.ts
@@ -10,9 +10,14 @@ export class Analyzer {
     this.tokens.push(...this.sentence.split(expression));
   }
 
+  private lowercase (): void {
+    this.tokens = this.tokens.map(token => token.toLowerCase());
+  }
+
   public transform (sentence: string, delimiterExpression?: RegExp): string[] {
     this.sentence = sentence;
     this.tokenize(delimiterExpression);
+    this.lowercase();
     const result = this.tokens;
     this.tokens = [];
     return result;
