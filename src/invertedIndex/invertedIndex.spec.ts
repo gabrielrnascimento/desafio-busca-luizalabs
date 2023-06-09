4d708011237bbc31979e3da5a293688246f3ffde
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2023-06-09 14:35:00 -0300

    test: move SUT creation to a factory method

--- src/invertedIndex/invertedIndex.spec.ts
+++ src/invertedIndex/invertedIndex.spec.ts
@@ -1,8 +1,19 @@
 import { InvertedIndex } from './invertedIndex';
 
+type SutTypes = {
+  sut: InvertedIndex
+};
+
+const makeSut = (): SutTypes => {
+  const sut = new InvertedIndex();
+  return {
+    sut
+  };
+};
+
 describe('InvertedIndex', () => {
   test('should create an empty Map', () => {
-    const invertedIndex = new InvertedIndex();
-    expect(invertedIndex.index).toStrictEqual(new Map());
+    const { sut } = makeSut();
+    expect(sut.index).toStrictEqual(new Map());
   });
 });
