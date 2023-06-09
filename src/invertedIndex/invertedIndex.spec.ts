06cbfb9afa42c952ce35b3cfbe9bb7d58543e0a6
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2023-06-09 16:36:54 -0300

    test: ensure InvertedIndex inserts document to index

--- src/invertedIndex/invertedIndex.spec.ts
+++ src/invertedIndex/invertedIndex.spec.ts
@@ -1,3 +1,4 @@
+import { Analyzer } from '../analyzer';
 import { InvertedIndex } from './invertedIndex';
 
 type SutTypes = {
@@ -5,7 +6,8 @@ type SutTypes = {
 };
 
 const makeSut = (): SutTypes => {
-  const sut = new InvertedIndex();
+  const analyzer = new Analyzer();
+  const sut = new InvertedIndex(analyzer);
   return {
     sut
   };
@@ -15,5 +17,18 @@ describe('InvertedIndex', () => {
   test('should create an empty Map', () => {
     const { sut } = makeSut();
     expect(sut.index).toStrictEqual(new Map());
+  });
+
+  test('should insert document to index', () => {
+    const { sut } = makeSut();
+    const mockDocument = {
+      title: 'any_title.txt',
+      content: 'any random content'
+    };
+
+    sut.insert(mockDocument.title, mockDocument.content);
+
+    expect(sut.index.get('random')?.has(mockDocument.title)).toBeTruthy();
+    expect(sut.index.get('any')?.has(mockDocument.title)).toBeTruthy();
   });
 });
