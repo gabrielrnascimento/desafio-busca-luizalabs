9ca3e98a2f92e2866fd8f8bd335857443d81fa2b
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2023-06-09 16:36:47 -0300

    feat: ensure InvertedIndex inserts document to index

--- src/invertedIndex/invertedIndex.ts
+++ src/invertedIndex/invertedIndex.ts
@@ -1,3 +1,5 @@
+import { type Analyzer } from '../analyzer';
+
 type DocumentTitle = string;
 type Term = string;
 
@@ -6,7 +8,17 @@ export type InvertedIndexType = Map<Term, Set<DocumentTitle>>;
 export class InvertedIndex {
   index: InvertedIndexType;
 
-  constructor () {
+  constructor (private readonly analyzer: Analyzer) {
     this.index = new Map<Term, Set<DocumentTitle>>();
+  }
+
+  public insert (title: string, content: string): void {
+    const tokens = this.analyzer.transform(content);
+    for (const token of tokens) {
+      if (!this.index.has(token)) {
+        this.index.set(token, new Set<DocumentTitle>());
+      }
+      this.index.get(token)?.add(title);
+    }
   }
 }
