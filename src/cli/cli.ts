78f843a5079dafcd9096fad6839cedf19bc5bce5
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2023-06-11 16:49:47 -0300

    refactor: move CLI output results to separate methods

--- src/cli/cli.ts
+++ src/cli/cli.ts
@@ -1,5 +1,10 @@
 import { type Term, type DocumentTitle } from '../utils/types';
 
+enum ResultsQuantity {
+  noResults = 0,
+  singleResult = 1
+}
+
 export class CLI {
   public handleInput (): string {
     const args = process.argv;
@@ -15,20 +20,33 @@ export class CLI {
     return searchTerm;
   }
 
+  private noResultsFound (searchTerm: Term): void {
+    console.log(`Não foi encontrada nenhuma ocorrência pelo termo "${searchTerm}"`);
+  }
+
+  private singleResultFound (searchTerm: Term, documents: DocumentTitle[]): void {
+    console.log(`Foi encontrada 1 ocorrência pelo termo "${searchTerm}"`);
+    console.log(`O arquivo que possui "${searchTerm}" é:`);
+    console.log(`${documents[0]}`);
+  }
+
+  private multipleResultFound (searchTerm: Term, documents: DocumentTitle[]): void {
+    console.log(`Foram encontradas ${documents.length} ocorrências pelo termo "${searchTerm}"`);
+    console.log(`Os arquivos que possuem "${searchTerm}" são:`);
+    documents.forEach(document => { console.log(document); });
+  }
+
   public handleOutput (searchTerm: Term, documents: DocumentTitle[]): void {
     const documentCount = documents.length;
-    if (documentCount === 0) {
-      console.log(`Não foi encontrada nenhuma ocorrência pelo termo "${searchTerm}"`);
-    }
-    if (documentCount === 1) {
-      console.log(`Foi encontrada 1 ocorrência pelo termo "${searchTerm}"`);
-      console.log(`O arquivo que possui "${searchTerm}" é:`);
-      console.log(`${documents[0]}`);
-    }
-    if (documentCount > 1) {
-      console.log(`Foram encontradas ${documentCount} ocorrências pelo termo "${searchTerm}"`);
-      console.log(`Os arquivos que possuem "${searchTerm}" são:`);
-      documents.forEach(document => { console.log(document); });
+    switch (documentCount) {
+      case ResultsQuantity.noResults:
+        this.noResultsFound(searchTerm);
+        break;
+      case ResultsQuantity.singleResult:
+        this.singleResultFound(searchTerm, documents);
+        break;
+      default:
+        this.multipleResultFound(searchTerm, documents);
     }
   }
 }
