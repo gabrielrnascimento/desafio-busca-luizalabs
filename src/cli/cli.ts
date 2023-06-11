597db9c16a77d575d417b0932904a03fa0e00981
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2023-06-11 13:54:21 -0300

    feat: ensure CLI displays an error if more than 3 arguments are provided

--- src/cli/cli.ts
+++ src/cli/cli.ts
@@ -1,8 +1,13 @@
 export class CLI {
   handleInput (): void {
-    const [, , searchTerm] = process.argv;
+    const args = process.argv;
+    const [, , searchTerm] = args;
     if (!searchTerm) {
       console.error('Insira um termo de busca');
+      process.exit(1);
+    }
+    if (args.length > 3) {
+      console.error('Número excessivo de parâmetros. Forneça apenas um termo de busca');
       process.exit(1);
     }
   }
