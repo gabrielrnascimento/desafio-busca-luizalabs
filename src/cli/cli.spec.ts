6be996766b624de3533317b7f67fede42332e831
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2023-06-11 16:23:41 -0300

    test: ensure CLI displays specific messages containing file count and file name in case only one document is provided

--- src/cli/cli.spec.ts
+++ src/cli/cli.spec.ts
@@ -1,3 +1,5 @@
+import { mockDirectoryFiles } from '../test/mocks';
+
 import { CLI } from './cli';
 
 type SutTypes = {
@@ -6,12 +8,15 @@ type SutTypes = {
   processExitSpy: jest.SpyInstance
   consoleLogSpy: jest.SpyInstance
   args: string[]
+  searchTerm: string
 };
 
 const makeSut = (args: string[] = ['node', 'any-file.ts', 'any search term']): SutTypes => {
   const consoleErrorSpy = jest.spyOn(console, 'error').mockImplementation();
   const processExitSpy = jest.spyOn(process, 'exit').mockImplementation();
   const consoleLogSpy = jest.spyOn(console, 'log').mockImplementation();
+
+  const searchTerm = 'any search term';
 
   process.argv = args;
   const sut = new CLI();
@@ -21,7 +26,8 @@ const makeSut = (args: string[] = ['node', 'any-file.ts', 'any search term']): S
     consoleErrorSpy,
     processExitSpy,
     consoleLogSpy,
-    args
+    args,
+    searchTerm
   };
 };
 
@@ -59,10 +65,17 @@ describe('CLI', () => {
   describe('handleOutput', () => {
     test('should display a specific message if no documents are provided', () => {
       const documentsFound: string[] = [];
-      const searchTerm = 'any search term';
-      const { sut, consoleLogSpy } = makeSut();
+      const { sut, consoleLogSpy, searchTerm } = makeSut();
       sut.handleOutput(searchTerm, documentsFound);
       expect(consoleLogSpy).toHaveBeenCalledWith(`Não foi encontrada nenhuma ocorrência pelo termo "${searchTerm}"`);
+    });
+    test('should display specific messages containg file count and file name in case only one document is provided', () => {
+      const documentsFound: string[] = ['only-file.txt'];
+      const { sut, consoleLogSpy, searchTerm } = makeSut();
+      sut.handleOutput(searchTerm, documentsFound);
+      expect(consoleLogSpy).toHaveBeenCalledWith(`Foi encontrada 1 ocorrência pelo termo "${searchTerm}"`);
+      expect(consoleLogSpy).toHaveBeenCalledWith(`O arquivo que possui "${searchTerm}" é:`);
+      expect(consoleLogSpy).toHaveBeenCalledWith(`${documentsFound[0]}`);
     });
   });
 });
