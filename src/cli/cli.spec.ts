11f8562296356558a6f41fa2d6e2d3a39aa0560e
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2023-06-11 16:09:49 -0300

    test: ensure CLI displays a specific message if no documents are provided

--- src/cli/cli.spec.ts
+++ src/cli/cli.spec.ts
@@ -4,11 +4,14 @@ type SutTypes = {
   sut: CLI
   consoleErrorSpy: jest.SpyInstance
   processExitSpy: jest.SpyInstance
+  consoleLogSpy: jest.SpyInstance
+  args: string[]
 };
 
-const makeSut = (args: string[]): SutTypes => {
+const makeSut = (args: string[] = ['node', 'any-file.ts', 'any search term']): SutTypes => {
   const consoleErrorSpy = jest.spyOn(console, 'error').mockImplementation();
   const processExitSpy = jest.spyOn(process, 'exit').mockImplementation();
+  const consoleLogSpy = jest.spyOn(console, 'log').mockImplementation();
 
   process.argv = args;
   const sut = new CLI();
@@ -16,7 +19,9 @@ const makeSut = (args: string[]): SutTypes => {
   return {
     sut,
     consoleErrorSpy,
-    processExitSpy
+    processExitSpy,
+    consoleLogSpy,
+    args
   };
 };
 
@@ -24,30 +29,40 @@ describe('CLI', () => {
   afterEach(() => {
     jest.clearAllMocks();
   });
-  test('should display an error if no search term is provided', () => {
-    const args = ['node', 'any-file.ts'];
-    const { sut, consoleErrorSpy, processExitSpy } = makeSut(args);
-    sut.handleInput();
+  describe('handleInput', () => {
+    test('should display an error if no search term is provided', () => {
+      const args = ['node', 'any-file.ts'];
+      const { sut, consoleErrorSpy, processExitSpy } = makeSut(args);
+      sut.handleInput();
 
-    expect(consoleErrorSpy).toHaveBeenCalledWith('Insira um termo de busca');
-    expect(processExitSpy).toHaveBeenCalledWith(1);
+      expect(consoleErrorSpy).toHaveBeenCalledWith('Insira um termo de busca');
+      expect(processExitSpy).toHaveBeenCalledWith(1);
+    });
+    test('should display an error if more than 3 arguments are provided', () => {
+      const args = ['node', 'any-file.ts', 'any search term', 'extra argument'];
+      const { sut, consoleErrorSpy, processExitSpy } = makeSut(args);
+      sut.handleInput();
+
+      expect(consoleErrorSpy).toHaveBeenCalledWith('Número excessivo de parâmetros. Forneça apenas um termo de busca');
+      expect(processExitSpy).toHaveBeenCalledWith(1);
+    });
+
+    test('should return search term on success', () => {
+      const { sut, args } = makeSut();
+      jest.restoreAllMocks();
+
+      const searchTerm = sut.handleInput();
+
+      expect(searchTerm).toBe(args[2]);
+    });
   });
-  test('should display an error if more than 3 arguments are provided', () => {
-    const args = ['node', 'any-file.ts', 'any search term', 'extra argument'];
-    const { sut, consoleErrorSpy, processExitSpy } = makeSut(args);
-    sut.handleInput();
-
-    expect(consoleErrorSpy).toHaveBeenCalledWith('Número excessivo de parâmetros. Forneça apenas um termo de busca');
-    expect(processExitSpy).toHaveBeenCalledWith(1);
-  });
-
-  test('should return search term on success', () => {
-    const args = ['node', 'any-file.ts', 'any search term'];
-    const { sut } = makeSut(args);
-    jest.restoreAllMocks();
-
-    const searchTerm = sut.handleInput();
-
-    expect(searchTerm).toBe(args[2]);
+  describe('handleOutput', () => {
+    test('should display a specific message if no documents are provided', () => {
+      const documentsFound: string[] = [];
+      const searchTerm = 'any search term';
+      const { sut, consoleLogSpy } = makeSut();
+      sut.handleOutput(searchTerm, documentsFound);
+      expect(consoleLogSpy).toHaveBeenCalledWith(`Não foi encontrada nenhuma ocorrência pelo termo "${searchTerm}"`);
+    });
   });
 });
