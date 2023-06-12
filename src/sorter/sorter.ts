3ea0f0e2b66acd3883752aff547af87714e4975f
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2023-06-12 16:30:18 -0300

    feat: ensure Sorter sorts an array in ascending numerical order

--- src/sorter/sorter.ts
+++ src/sorter/sorter.ts
@@ -1,7 +1,45 @@
-import { type DocumentTitle } from '../utils/types';
+import { type Term, type DocumentTitle } from '../utils/types';
 
 export class Sorter {
+  private splitIntoWordsAndNumbers (documentTitle: DocumentTitle): Term[] {
+    const regex = /(\d+[\w-]*)|([^\d-]+)/g;
+    return documentTitle.match(regex) ?? [];
+  }
+
+  private isNonNumeric (token: Term): boolean {
+    return isNaN(parseInt(token));
+  }
+
+  private compareStrings (firstDocumentToken: Term, secondDocumentToken: string): number {
+    return firstDocumentToken.localeCompare(secondDocumentToken);
+  }
+
+  private compareNumbers (firstDocumentToken: Term, secondDocumentToken: string): number {
+    return parseInt(firstDocumentToken) - parseInt(secondDocumentToken);
+  }
+
   public sort (documents: DocumentTitle[]): DocumentTitle[] {
-    return documents.sort();
+    return documents.sort((firstDocument, secondDocument) => {
+      const firstDocumentTokens = this.splitIntoWordsAndNumbers(firstDocument);
+      const secondDocumentTokens = this.splitIntoWordsAndNumbers(secondDocument);
+
+      const minLength = Math.min(firstDocumentTokens.length, secondDocumentTokens.length);
+      for (let i = 0; i < minLength; i++) {
+        const firstDocumentToken = firstDocumentTokens[i];
+        const secondDocumentToken = secondDocumentTokens[i];
+
+        if (this.isNonNumeric(firstDocumentToken) || this.isNonNumeric(secondDocumentToken)) {
+          const result = this.compareStrings(firstDocumentToken, secondDocumentToken);
+          if (result !== 0) {
+            return result;
+          }
+        } else {
+          const result = this.compareNumbers(firstDocumentToken, secondDocumentToken);
+          return result;
+        }
+      }
+
+      return firstDocument.length - secondDocument.length;
+    });
   }
 }
