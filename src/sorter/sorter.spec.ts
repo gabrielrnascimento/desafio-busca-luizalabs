084e15e204edf8e8c23c7ecfaff3e4e73e06ba17
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2023-06-12 16:31:35 -0300

    test: ensure Sorter sorts an array with empty string and repeated items in acscending order

--- src/sorter/sorter.spec.ts
+++ src/sorter/sorter.spec.ts
@@ -52,4 +52,34 @@ describe('Sorter', () => {
     const result = sorter.sort(unsortedMovies);
     expect(result).toStrictEqual(sortedMovies);
   });
+
+  test('should sort an array with empty strings and repeated items in ascending order', () => {
+    const unsortedMovies = [
+      '2-days-in-the-valley.txt',
+      '13-lead-soldiers.txt',
+      '1-something.txt',
+      '1-something.txt',
+      '1001-arabian-nights.txt',
+      '2nd-take.txt',
+      '',
+      '40-minutes-of-hell.txt',
+      '2012-the-odyssey.txt'
+    ];
+
+    const sortedMovies = [
+      '',
+      '1-something.txt',
+      '1-something.txt',
+      '2-days-in-the-valley.txt',
+      '2nd-take.txt',
+      '13-lead-soldiers.txt',
+      '40-minutes-of-hell.txt',
+      '1001-arabian-nights.txt',
+      '2012-the-odyssey.txt'
+    ];
+
+    const sorter = new Sorter();
+    const result = sorter.sort(unsortedMovies);
+    expect(result).toStrictEqual(sortedMovies);
+  });
 });
